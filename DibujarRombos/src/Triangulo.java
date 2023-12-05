6289b8603e65e9f036f3a13359f83ada0a92199a
LUIS ALBERTO DIAZ SANCHEZ <ESQ49289@educastur.es>
2023-12-05 22:13:52 +0100

    Añadido Triángulo inicial

---
+++ DibujarRombos/src/Triangulo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Altura del triángulo");
+		int altura = in.nextInt();
+		for (int i=1;i<=altura;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
