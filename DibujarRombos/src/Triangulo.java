f673e5341c6b8ae9b921e178ca190e1c387323f8
LUIS ALBERTO DIAZ SANCHEZ <100797189+albertdods@users.noreply.github.com>
2023-12-05 22:16:12 +0100

    Update Triangulo.java

--- DibujarRombos/src/Triangulo.java
+++ DibujarRombos/src/Triangulo.java
@@ -6,6 +6,7 @@ public class Triangulo {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Altura del triángulo");
 		int altura = in.nextInt();
+		System.out.println("Triangulo de altura "+altura);
 		for (int i=1;i<=altura;i++) {
 			for (int j=1;j<=i;j++) {
 				System.out.print("*");
